5403580c03c23edbda1b9543306c0b18c5f4b78b
Peter Chen <abc@abc.com>
2023-03-10 09:04:34 -0800

    Get a 4-digit number from the user

--- personal_website/javascript/javascript.js
+++ personal_website/javascript/javascript.js
@@ -1,6 +1,22 @@
-const courseList = [firstCourse, secondCourse, thridCourse]
+let firstCourse = {code:"A1620", name:"Web Fundamental Technologies"};
+let secondCourse = {code:"A1515", name:"Scripting for IT"};
+let thridCourse = {code:"A1630", name:"Database Systems"};
 
-let firstCourse = {code:"A1620", name:"Web Fundamental Technologies"}
-let secondCourse = {code:"A1515", name:"Scripting for IT"}
-let thridCourse = {code:"A1630", name:"Database Systems"}
+const courseList = [firstCourse, secondCourse, thridCourse];
+
+let answer
+
+do {
+    answer = prompt ('type 4-digit number');
+    console.log(answer.length === 4)
+}
+while (isNaN(answer) || !(answer.length === 4)); //(isNaN(answer) && (2 === 3));
+
+
+// do {
+//     answer = prompt ("Are you human? ");
+//     // console.log(answer)
+// }
+// while (!isNaN(answer));
+// alert(answer);
 

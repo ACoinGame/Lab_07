060190e7c55f57b784b889abfbacccec2a4c2eee
Peter Chen <abc@abc.com>
2023-03-09 19:30:07 -0800

    adds a constant array containing three objects and define them as well

---
+++ personal_website/javascript/javascript.js
@@ -0,0 +1,6 @@
+const courseList = [firstCourse, secondCourse, thridCourse]
+
+let firstCourse = {code:"A1620", name:"Web Fundamental Technologies"}
+let secondCourse = {code:"A1515", name:"Scripting for IT"}
+let thridCourse = {code:"A1630", name:"Database Systems"}
+

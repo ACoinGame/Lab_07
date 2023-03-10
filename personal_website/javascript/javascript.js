8d135a9e80607cb18a1ebc1fb86c69437b77fc9a
Peter Chen <abc@abc.com>
2023-03-10 11:19:26 -0800

    Adds a loop to check if user's input is in the courselist. if yes, addds it the courselist. if not, log success

--- personal_website/javascript/javascript.js
+++ personal_website/javascript/javascript.js
@@ -1,6 +1,6 @@
-let firstCourse = {code:"A1620", name:"Web Fundamental Technologies"};
-let secondCourse = {code:"A1515", name:"Scripting for IT"};
-let thridCourse = {code:"A1630", name:"Database Systems"};
+let firstCourse = {code:"ACIT 1620", name:"Web Fundamental Technologies"};
+let secondCourse = {code:"ACIT 1515", name:"Scripting for IT"};
+let thridCourse = {code:"ACIT 1630", name:"Database Systems"};
 
 const courseList = [firstCourse, secondCourse, thridCourse];
 
@@ -10,7 +10,18 @@ do {
     answer = prompt ('type 4-digit number');
     console.log(answer.length === 4)
 }
-while (isNaN(answer) || !(answer.length === 4)); //(isNaN(answer) && (2 === 3));
+while (isNaN(answer) || !(answer.length === 4)); 
+
+for (let i = 0; i < courseList.length; i++) {
+    if (courseList[i].code.includes(answer)) {
+        console.log(`Yes I am taking the course: ${courseList[i].code} - ${courseList[i].name}`)
+    } else if (i + 1 === Object.keys(courseList).length) {
+        courseList.push({code: `ACIT ${answer}`, name: null})
+        console.log(`Success`)
+        break
+    }
+}
+
 
 
 // do {
